dbaf91ce51e8e0c242f0aa557f842902e644bf0d
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2026-04-26 09:49:09 -0300

    2026.000006.1: scaffold cmdb-service project and configure dependencies

---
+++ cmdb-service/src/main/java/com/zenandops/cmdb/infrastructure/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Infrastructure layer: adapters for REST, MongoDB, Kafka, and file import.
+ * This layer implements the ports defined in the application layer.
+ */
+package com.zenandops.cmdb.infrastructure;
